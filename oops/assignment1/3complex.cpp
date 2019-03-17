f3c40fecd86dd836ba319eaea7950291bc9ab4de
HimaMynampati <35256374+HimaMynampati@users.noreply.github.com>
2019-03-17 07:59:24 -0700

    Update 3complex.cpp

--- oops/assignment1/3complex.cpp
+++ oops/assignment1/3complex.cpp
@@ -23,16 +23,23 @@ public:
 	{
 		real = c1.real + c2.real;
 		img = c1.img + c2.img;
-		cout << real << "+" << img<<"i"<<endl;
+		cout <<"Sum of the given two comples numbers is :" <<real << "+" << img<<"i"<<endl;
 	}
 	void mul_complex(Complex &c1, Complex &c2)
 	{
+		cout<<"Product of the given two complex numbers is :";
 		cout << ((c1.real*c2.real) - (c1.img*c2.img)) << "+" << ((c1.real)*(c2.img) + (c1.img)*(c2.real))<<"i";
 	}
 };
-void main()
+int main()
 {
-	Complex c1(9, 5), c2(4, 6), c3;
+	float real1,real2,img1,img2;
+	cout<<"enter First numbers real and imaginary parts  ";
+	cin>> real1>>img1;
+	cout<<endl<<"enter Second numbers real and imaginary parts  ";
+	cin>> real2>>img2;
+	Complex c1(real1, img1), c2(real2, img2), c3;
 	c3.add_complex(c1, c2);
 	c3.mul_complex(c1, c2);
+	return 0;
 }
